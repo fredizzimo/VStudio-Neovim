60cab3caddeb2809c1e094b3ecbec4ca366f81d4
Fred Sundvik <fsundvik@gmail.com>
2020-12-24 17:10:44 +0200

    Add a control for the document group

---
+++ UI/NeovimDocumentGroupControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace vstudio_neovim.UI
+{
+    /// <summary>
+    /// Interaction logic for NeovimDocumentGroupControl.xaml
+    /// </summary>
+    public partial class NeovimDocumentGroupControl : UserControl
+    {
+        public NeovimDocumentGroupControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- vstudio_neovimPackage.cs
+++ vstudio_neovimPackage.cs
@@ -6,7 +6,8 @@ using System.Runtime.InteropServices;
 using System.Threading;
 using Task = System.Threading.Tasks.Task;
 using Microsoft.VisualStudio.PlatformUI.Shell;
-
+using System.Windows;
+using System.Reflection;
 
 namespace vstudio_neovim
 {
@@ -53,16 +54,31 @@ namespace vstudio_neovim
         /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
+            MergeResources();
             //Create Editor Factory. Note that the base Package class will call Dispose on it.
             base.RegisterEditorFactory(new EditorFactory(this));
             ViewElementFactory.Current = new NeovimViewElementFactory(ViewElementFactory.Current);
-
 
         // When initialized asynchronously, the current thread may be a background thread at this point.
         // Do any initialization that requires the UI thread after switching to the UI thread.
         await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
             await MainWindowCommand.InitializeAsync(this);
         }
+
+        private void MergeResources()
+        {
+            if (Application.Current == null)
+                throw new InvalidOperationException("We need an application set.");
+            ResourceDictionary resourceDictionary = LoadResource<ResourceDictionary>("UI/DataTemplates.xaml");
+            Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+        }
+
+        private T LoadResource<T>(string xamlName)
+        {
+            var uri = new Uri(Assembly.GetExecutingAssembly().GetName().Name + ";component/" + xamlName, UriKind.Relative);
+            return (T)Application.LoadComponent(uri);
+        }
+
 
         #endregion
     }
