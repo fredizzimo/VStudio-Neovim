cb3e54b3b301e8989f9de81b53dbb35549dc22ce
Fred Sundvik <fsundvik@gmail.com>
2021-01-06 21:16:54 +0200

    Add TextViewCreationListener

---
+++ TextViewCreationListener.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VStudioNeovim
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Editable)]
+    public class TextViewCreationListener : IWpfTextViewCreationListener
+    {
+        public void TextViewCreated(IWpfTextView textView)
+        {
+        }
+    }
+}
