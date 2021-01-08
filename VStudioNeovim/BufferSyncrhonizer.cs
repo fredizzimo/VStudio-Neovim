25448f4781fcfa322ce7716fb99ac5ea6d0a73b9
Fred Sundvik <fsundvik@gmail.com>
2021-01-08 20:47:59 +0200

    Add Buffer and BufferSyncrhonizer classes
    Just dummies for now

---
+++ VStudioNeovim/BufferSyncrhonizer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.Text;
+
+namespace VStudioNeovim
+{
+    public class BufferSyncrhonizer
+    {
+        public BufferSyncrhonizer(ITextBufferFactoryService textBufferFactoryService)
+        {
+            _textBufferFactoryService = textBufferFactoryService;
+            _textBufferFactoryService.TextBufferCreated += TextBufferCreated;
+        }
+
+        private void TextBufferCreated(object sender, TextBufferCreatedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private readonly ITextBufferFactoryService _textBufferFactoryService;
+    }
+}
